524ec89295585da52fa7491dc1d51f1c5012cb66
Asier Zamorano Villalba <asierz86@gmail.com>
2025-04-28 11:46:01 +0200

    empezando

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,21 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+
+class Bureaucrat
+{
+    private:
+        const std::string name;
+        int grade;
+    
+    public:
+        
+}
+
+
+
+#endif
